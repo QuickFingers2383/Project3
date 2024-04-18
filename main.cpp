ddda23dec95b990752481c25332bdbbe361d049c
John Wang <55559153+QuickFingers2383@users.noreply.github.com>
2024-04-18 14:15:08 -0400

    Created B+ tree header file and wrote B+ tree insertion function.  - Need to test insertion function.

--- main.cpp
+++ main.cpp
@@ -5,21 +5,12 @@ int main() {
     // Create a B+ tree with degree 3
     BPlusTree bptree(3);
 
-    // Insert housing data into the B+ tree
+    // testing
     House house1 = {250000, 2000, 2,  2.5,   2005};
     House house2 = {320000, 2200, 1,  2.5,  2010};
-    // Insert more houses as needed
 
     bptree.insert(house1);
     bptree.insert(house2);
-    // Insert more houses as needed
-
-    // Search for a house based on certain criteria
-    House searched_house = bptree.search(320000);
-    // Print the searched house attributes
-
-    // Remove a house from the B+ tree
-    bptree.remove(250000);
 
     return 0;
 }
