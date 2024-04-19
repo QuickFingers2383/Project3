f26c45b0f80a208a8e73ada809a1bdd8938ffd5f
John Wang <55559153+QuickFingers2383@users.noreply.github.com>
2024-04-19 16:26:41 -0400

    Added some comments

--- main.cpp
+++ main.cpp
@@ -5,15 +5,20 @@
 #include <string>
 #include "b+tree.h"
 
+// CSV parsing function
 vector<House> parseCSV(const string& filename) {
+    // initialize struct
     vector<House> houses;
     ifstream file(filename);
+    // checks if file exists
     if (!file.is_open()) {
         cerr << "Error opening file: " << filename << endl;
         return houses;
     }
+    // skips first line, which are column titles
     string line;
     getline(file, line);
+    // Transfers CSV data into a vector of houses
     while (getline(file, line)) {
         stringstream ss(line);
         string cell;
@@ -40,6 +45,7 @@ int main() {
     // Create a B+ tree
     BPlusTree bptree(3);
 
+    // Only the absolute path works, not sure why. Change this path in your computer!
     string filename = R"(C:\Users\yjwan\CLionProjects\Project3\data\house_data1.csv)";
     vector<House> houses = parseCSV(filename);
     if (houses.empty()) {
