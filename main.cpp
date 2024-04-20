78646c267d93d0afab0f1329f78ea65cea829975
John Wang <55559153+QuickFingers2383@users.noreply.github.com>
2024-04-20 16:02:18 -0400

    Added a basic interface and timers for search function comparisons. Waiting for search function implementations.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <string>
 #include "b+tree.h"
+#include <chrono>
+
+using namespace std;
 
 // CSV parsing function
 vector<House> parseCSV(const string& filename) {
@@ -41,6 +44,51 @@ vector<House> parseCSV(const string& filename) {
     return houses;
 }
 
+void timer(int search_type) {
+    auto bp_start = chrono::high_resolution_clock::now();
+    // run b+ tree search function
+    // TODO: Add search functions for each type
+    if(search_type == 1) {
+
+    } else if(search_type == 2) {
+
+    } else if(search_type == 3) {
+
+    } else if(search_type == 4) {
+
+    } else if(search_type == 5) {
+
+    }
+
+    auto bp_stop = chrono::high_resolution_clock::now();
+
+    // display stuff
+    auto bp_duration = chrono::duration_cast<chrono::microseconds>(bp_stop-bp_start);
+    cout << "Duration: " << bp_duration.count() << endl;
+
+    auto h_start = chrono::high_resolution_clock::now();
+    // run b+ tree search function
+    // TODO: Add search functions for each type
+    if(search_type == 1) {
+
+    } else if(search_type == 2) {
+
+    } else if(search_type == 3) {
+
+    } else if(search_type == 4) {
+
+    } else if(search_type == 5) {
+
+    }
+
+    auto h_stop = chrono::high_resolution_clock::now();
+
+    // TODO: Display search results
+    auto h_duration = chrono::duration_cast<chrono::microseconds>(h_stop-h_start);
+    cout << "Duration: " << h_duration.count() << endl;
+
+}
+
 int main() {
     // Create a B+ tree
     BPlusTree bptree(3);
@@ -57,6 +105,53 @@ int main() {
     for (const auto& house : houses) {
         bptree.insert(house);
     }
+
+    cout << "-----------------------------------------------" << endl;
+    cout << "Welcome to Cillow! The search engine for homes!" << endl;
+    cout << "-----------------------------------------------" << endl;
+
+    int option;
+    string num_bedrooms, num_bathrooms, num_sqft, year, price;
+
+    while(true) {
+        cout << "Choose what option you want: " << endl;
+        cout << "Search by Square Feet (1)\n"
+                "Search by bedrooms (2)\n"
+                "Search by bathrooms (3)\n"
+                "Search by year built (4)\n"
+                "Search by price (5)\n"
+                "Quit (6) \n" << endl;
+        cin >> option;
+        if(option == 1) {
+            cout << "Square feet: 1.5k-2k, 2k-2.5k, 2.5k-3k, 3k-3.5k" << endl;
+            cin >> num_sqft;
+            timer(1);
+        }
+        if(option == 2) {
+            cout << "Number of bedrooms: 2, 3, 4, 5, 6" << endl;
+            cin >> num_bedrooms;
+            timer(2);
+        }
+        if(option == 3) {
+            cout << "Number of bathrooms: 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8" << endl;
+            cin >> num_bathrooms;
+            timer(3);
+        }
+        if(option == 4) {
+            cout << "Year built: 1980-1985, 1985-1990, 1990-1995, 1995-2000, 200-2005, 2005-2010, 2010-2015, 2015+" << endl;
+            cin >> year;
+            timer(4);
+        }
+        if(option == 5) {
+            cout << "Price: 150k-200k, 200k-250k, 250k-300k, 300k-350k. 350k-400k, 400k-450k, 450k-500k, 500k-550k, 550k-600k, 600k+" << endl;
+            cin >> price;
+            timer(5);
+        }
+        if(option == 6) {
+            break;
+        }
+    }
+    // run search functions for parameters then display
 
     return 0;
 }
